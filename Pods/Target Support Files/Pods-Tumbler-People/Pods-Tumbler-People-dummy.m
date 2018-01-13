7fb39d0d4bdfcf023f0a71d31905a4df4f40b263
EliScherrer <escherre@purdue.edu>
2018-01-13 15:42:56 -0500

    more setup

---
+++ Pods/Target Support Files/Pods-Tumbler-People/Pods-Tumbler-People-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumbler_People : NSObject
+@end
+@implementation PodsDummy_Pods_Tumbler_People
+@end
---
+++ Pods/Target Support Files/Pods-Tumbler-PeopleUITests/Pods-Tumbler-PeopleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tumbler_PeopleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tumbler_PeopleUITests
+@end

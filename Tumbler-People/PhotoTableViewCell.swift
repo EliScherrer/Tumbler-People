199365ad558e0de78aad38fc0f67a277bab39aef
EliScherrer <escherre@purdue.edu>
2018-01-14 19:25:59 -0500

    finished up main part

---
+++ Tumbler-People/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  Tumbler-People
+//
+//  Created by Eli Scherrer on 1/14/18.
+//  Copyright © 2018 Eli Scherrer. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
